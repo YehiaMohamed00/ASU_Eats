98c0f8100048d604a7a2dd987e6b55c35aa2f158
YehiaMohamed00 <esc.yehia.mohamed1@gmail.com>
2022-12-09 04:16:28 +0200

    additions: - cart recycler view - cart view - payment view - order tracking view - gradient xml template - minor changes to menu_view

--- app/src/main/java/com/example/asueats/MainActivity.java
+++ app/src/main/java/com/example/asueats/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.restaurants_view);
+        setContentView(R.layout.order_track_view);
     }
 }
